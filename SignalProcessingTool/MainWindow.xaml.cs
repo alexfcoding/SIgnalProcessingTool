06f556d7896b788518da93572c24a705d3302a04
alexfcoding <fedotov1609@gmail.com>
2019-07-03 05:39:55 +0600

    Acoustic model class

--- SignalProcessingTool/MainWindow.xaml.cs
+++ SignalProcessingTool/MainWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace SignalProcessingTool
     using OxyPlot.Series;
     using OxyPlot.Axes;
     using System.Collections.ObjectModel;
-    
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -43,81 +43,39 @@ namespace SignalProcessingTool
 
         public void CalculateImpulse()
         {
-            double pi = Math.PI;
-            double func = 0;
+            AcousticModel Model1 = new AcousticModel();
+
+            Model1.Pi = Math.PI;
+            Model1.Diameter = 0.038;
+            Model1.Thickness = 0.003;
+            Model1.Density = 7800;
+            Model1.PipeLength = 10;
             
-            double diameter = 0.038;
-            double thickness = 0.003;
-            double density = 7800;
-            double speed = 0;
-            double area = 0.3;
-            double pipeLength = 10;
-            double xCoordinate = pipeLength/1.6;
-            
-            double a1 = 10;
-            double a2 = 10;
-            double a3 = 5000000;
-            double signalDuration = 1;
-            double tc = 0.00004;
-            double ti = 0.000022;
-            double Fd = 44100;
+            Model1.A1 = 10;
+            Model1.A2 = 10;
+            Model1.A3 = 5000000;
+            Model1.SignalDuration = 1;
+            Model1.Tc = 0.00004;
+            Model1.Ti = 0.000022;
+            Model1.Fd1 = 44100;
+            Model1.ModNumber = 500;
+            Model1.Cn1 = new double[500];
+            Model1.Delta = new double[500];
+            Model1.W1 = new double[500];
+            Model1.Oi1 = new double[500];
+            Model1.Sum = new double[44100];
 
-            double modNumber = 500;
+            Model1.XCoordinate = Model1.PipeLength / 1.6;
+            Model1.E1 = 200 * Math.Pow(10, 9);
+            Model1.J1 = Model1.Pi * Math.Pow(Model1.Diameter, 3) * (double) Model1.Thickness / 8;
+            Model1.Mass1 = Model1.Density * Model1.Pi * Model1.Diameter * Model1.Thickness;
+            Model1.A4 = Model1.E1 * Model1.J1 / Model1.Mass1;
 
-            double[] Cn = new double[500];
-            double[] delta = new double[500];
-            double[] W = new double[500];
-            double[] Oi = new double[500];
-            double[] sum = new double[88200];
+            Model1.ComputeModel();
 
-            double E = 200 * Math.Pow(10, 9);
-            double J = pi * Math.Pow(diameter, 3) * (double)thickness / 8;
-            double Mass = density * pi * diameter * thickness;
-            double a4 = E * J / Mass;
-           
-            int k = 0;
+            DrawImpulse(Model1.SignalDuration * Model1.Fd1 - 1, Model1.Sum);
+            //Plot2.Series[0].ItemsSource = Model1.Points;
 
-            for (int i = 1; i < modNumber; i = i + 2)
-            {
-                Cn[k] = pi / 2 * (2 * i + 1);
-                delta[k] = 0.5 * (a1 * Math.Pow(i, 1) * Math.Pow(pi, 4) / Math.Pow(pipeLength, 4) + a2);
-                W[k] = Math.Sqrt(a3 + a4 * Math.Pow(i, 4) * Math.Pow(pi, 4) / Math.Pow(pipeLength, 4));
-                Oi[k] = Math.Sqrt(Math.Pow(W[k], 2) - Math.Pow(delta[k], 2));
-                k++;
-            }
-
-            k = 0;
-
-            double m1, m2, m3, rotator;
-
-            for (int i = 1; i < signalDuration * Fd; i++)
-            {
-                for (int j = 1; j < modNumber; j = j + 2)
-                {
-                    rotator = Math.Pow(-1, (j - 1) / 2);
-
-                    m1 = 2 * Math.Exp(delta[k] * tc) * delta[k] * pi / tc;
-
-                    m2 = pi * Math.Exp(delta[k] * tc) / tc / Oi[k] * (2 * Math.Pow(delta[k], 2) - Math.Pow(W[k], 2) + Math.Pow(pi, 2) / Math.Pow(tc, 2));
-
-                    m3 = Oi[k] * tc;
-
-                    sum[i] = sum[i] + rotator * Math.Exp(-delta[k] * ti) * Math.Sin(j * pi * xCoordinate / pipeLength) /
-                    (Math.Pow((Math.Pow(W[k], 2) - Math.Pow(pi, 2) / Math.Pow(tc, 2)), 2) + 4 * Math.Pow(pi, 2) * Math.Pow(delta[k], 2) / Math.Pow(tc, 2)) *
-
-                    ((m1 * Math.Cos(m3) - m2 * Math.Sin(m3) + 2 * pi * delta[k] / tc) * Math.Cos(Oi[k] * ti) + (m2 * Math.Cos(m3) + m1 * Math.Sin(m3) + 
-                    pi / tc / Oi[k] * (2 * Math.Pow(delta[k], 2) - Math.Pow(W[k], 2) +
-                    Math.Pow(pi, 2) / Math.Pow(tc, 2))) * Math.Sin(Oi[k] * ti));
-                        
-                    k++;                   
-                }
-
-                k = 0;
-               
-                ti = ti + (double) (1 / Fd);
-            }
-
-            DrawImpulse(signalDuration * Fd - 1, sum);
         }
 
         void DrawImpulse(double xPointsCount, double[] valuesArray)
@@ -139,6 +97,109 @@ namespace SignalProcessingTool
             Plot1.InvalidatePlot(true);
         }
 
+
+    }
+
+    public class AcousticModel
+    {
+        public AcousticModel() { }
+
+        public AcousticModel(double diameter, double thickness, double density, double pipeLength, double xCoordinate, double a1, double a2, double a3, double signalDuration, double tc, double ti, double fd1, double modNumber, double[] cn1, double[] delta, double[] w1, double[] oi1, double[] sum, double e1, double j1, double mass1, double a4, double pi)
+        {
+            Diameter = diameter;
+            Thickness = thickness;
+            Density = density;
+            PipeLength = pipeLength;
+            XCoordinate = xCoordinate;
+            A1 = a1;
+            A2 = a2;
+            A3 = a3;
+            SignalDuration = signalDuration;
+            Tc = tc;
+            Ti = ti;
+            Fd1 = fd1;
+            ModNumber = modNumber;
+            Cn1 = cn1;
+            Delta = delta;
+            W1 = w1;
+            Oi1 = oi1;
+            Sum = sum;
+            E1 = e1;
+            J1 = j1;
+            Mass1 = mass1;
+            A4 = a4;
+            Pi = pi;
+        }
+
+        public void ComputeModel()
+        {
+            int k = 0;
+
+            for (int i = 1; i < ModNumber; i = i + 2)
+            {
+                Cn1[k] = Pi / 2 * (2 * i + 1);
+                Delta[k] = 0.5 * (A1 * Math.Pow(i, 1) * Math.Pow(Pi, 4) / Math.Pow(PipeLength, 4) + A2);
+                W1[k] = Math.Sqrt(A3 + A4 * Math.Pow(i, 4) * Math.Pow(Pi, 4) / Math.Pow(PipeLength, 4));
+                Oi1[k] = Math.Sqrt(Math.Pow(W1[k], 2) - Math.Pow(Delta[k], 2));
+                k++;
+            }
+
+            k = 0;
+
+            double m1, m2, m3, rotator;
+
+            for (int i = 1; i < SignalDuration * Fd1; i++)
+            {
+                for (int j = 1; j < ModNumber; j = j + 2)
+                {
+                    rotator = Math.Pow(-1, (j - 1) / 2);
+
+                    m1 = 2 * Math.Exp(Delta[k] * Tc) * Delta[k] * Pi / Tc;
+
+                    m2 = Pi * Math.Exp(Delta[k] * Tc) / Tc / Oi1[k] * (2 * Math.Pow(Delta[k], 2) - Math.Pow(W1[k], 2) + Math.Pow(Pi, 2) / Math.Pow(Tc, 2));
+
+                    m3 = Oi1[k] * Tc;
+
+                    Sum[i] = Sum[i] + rotator * Math.Exp(-Delta[k] * Ti) * Math.Sin(j * Pi * XCoordinate / PipeLength) /
+                    (Math.Pow((Math.Pow(W1[k], 2) - Math.Pow(Pi, 2) / Math.Pow(Tc, 2)), 2) + 4 * Math.Pow(Pi, 2) * Math.Pow(Delta[k], 2) / Math.Pow(Tc, 2)) *
+
+                    ((m1 * Math.Cos(m3) - m2 * Math.Sin(m3) + 2 * Pi * Delta[k] / Tc) * Math.Cos(Oi1[k] * Ti) + (m2 * Math.Cos(m3) + m1 * Math.Sin(m3) +
+                    Pi / Tc / Oi1[k] * (2 * Math.Pow(Delta[k], 2) - Math.Pow(W1[k], 2) + Math.Pow(Pi, 2) / Math.Pow(Tc, 2))) * Math.Sin(Oi1[k] * Ti));
+
+                    k++;
+                }
+
+                k = 0;
+                
+                Ti = Ti + (double)(1 / Fd1);
+            }
+
+            //DrawImpulse(signalDuration * Fd - 1, sum);
+        }
+
+        public double Diameter { get; set; }
+        public double Thickness { get; set; }
+        public double Density { get; set; }
+        public double PipeLength { get; set; }
+        public double XCoordinate { get; set; }
+        public double A1 { get; set; }
+        public double A2 { get; set; }
+        public double A3 { get; set; }
+        public double SignalDuration { get; set; }
+        public double Tc { get; set; }
+        public double Ti { get; set; }
+        public double Fd1 { get; set; }
+        public double ModNumber { get; set; }
+        public double[] Cn1 { get; set; }
+        public double[] Delta { get; set; }
+        public double[] W1 { get; set; }
+        public double[] Oi1 { get; set; }
+        public double[] Sum { get; set; }
+        public double E1 { get; set; }
+        public double J1 { get; set; }
+        public double Mass1 { get; set; }
+        public double A4 { get; set; }
+        public double Pi { get; set; }
     }
 
     public class PointClass
